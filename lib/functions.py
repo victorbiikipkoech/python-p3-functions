c5547a485e6f6f589e8e898af2f7830a431bff81
VICTOR <victor.kipkoech@student.moringaschool.com>
2023-12-06 11:26:51 +0300

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,32 @@
-#!/usr/bin/env python3
-
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    
+    return number / 2
+
+# Test the functions
+greet_programmer()  # Outputs: "Hello, programmer!"
+greet("Naureen")    # Outputs: "Hello, Naureen!"
+greet_with_default()          # Outputs: "Hello, programmer!"
+greet_with_default("Sunny")   # Outputs: "Hello, Sunny!"
+
+sum_result = add(1, 2)
+print(sum_result)  # Outputs: 3
+
+halve_result = halve(4)
+print(halve_result)  # Outputs: 2
+
+invalid_result = halve("two")
+print(invalid_result)  # Outputs: None
